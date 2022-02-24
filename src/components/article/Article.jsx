4f1bb80a32ba2408ffb31188acc3c64e308e2f40
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-02-24 16:51:11 +0530

    blog section completed and article component done

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './article.css';
 
-function Article() {
+function Article({ imageUrl, date, title }) {
   return (
-    <div>Article</div>
+    <div className='gpt3__blog-container_article'>
+      <div className="gpt3__blog-container_article-image">
+        <img src={imageUrl} alt="blog image" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
