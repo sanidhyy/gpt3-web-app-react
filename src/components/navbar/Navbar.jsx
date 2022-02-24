546e64998dfc2f82a0f0539aa310dee3839adc72
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-02-24 17:27:49 +0530

    Fixed scrolling issue when clicking on links.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
   const Menu = () => (
     <>
       <p><a href="#home">Home</a></p>
-      <p><a href="#gpt3">What is GPT3?</a></p>
+      <p><a href="#wgpt3">What is GPT3?</a></p>
       <p><a href="#possibility">Open AI</a></p>
       <p><a href="#features">Case Studies</a></p>
       <p><a href="#blog">Library</a></p>
